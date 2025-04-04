094fffbed754a8733e1667fca2523b1d40be4b00
Elon Aseneka <elonsantos63@gmail.com>
2025-04-05 00:01:12 +0300

    Fix some major bugs on file purchase

--- services/files/src/utils/files.rs
+++ services/files/src/utils/files.rs
@@ -1,6 +1,12 @@
 use std::io::{Error, ErrorKind};
 
-use lib::utils::{custom_traits::AsSurrealClient, models::PurchaseFileDetails};
+use lib::{
+    integration::foreign_key::add_foreign_key_if_not_exists,
+    utils::{
+        custom_traits::AsSurrealClient,
+        models::{ForeignKey, PurchaseFileDetails, User},
+    },
+};
 
 use crate::graphql::schemas::general::UploadedFile;
 
@@ -76,35 +82,41 @@ pub async fn purchase_file<T: Clone + AsSurrealClient>(
     db: &T,
     purchase_details: PurchaseFileDetails,
 ) -> Result<bool, Error> {
-    let _purchase_file_query = db
+    let user_fk = ForeignKey {
+        table: "user_id".into(),
+        column: "user_id".into(),
+        foreign_key: purchase_details.buyer_id.into(),
+    };
+
+    let buyer_result: Option<User> = add_foreign_key_if_not_exists(db, user_fk).await;
+
+    let mut purchase_file_query = db
         .as_client()
         .query(
             "
-        BEGIN TRANSACTION;
-        LET $file_thing = type::thing($file_id);
-        LET $user = (SELECT * FROM ONLY user_id WHERE user_id = $user_id LIMIT 1);
-
-        LET $purchased_file = (RELATE $user -> bought_file -> $file_thing RETURN AFTER);
-
-        RETURN $purchased_file;
-        COMMIT TRANSACTION;
+            BEGIN TRANSACTION;
+            LET $file_thing = type::thing($file_id);
+            LET $user = (SELECT * FROM ONLY user_id WHERE user_id = $user_id LIMIT 1);
+            LET $purchased_file = (RELATE $user->bought_file->$file_thing RETURN AFTER);
+            RETURN $user_id;
+            COMMIT TRANSACTION;
         ",
         )
         .bind((
             "file_id",
             format!("file:{}", purchase_details.file_id.clone()),
         ))
-        .bind(("user_id", purchase_details.buyer_id.clone()))
+        .bind(("user_id", buyer_result.unwrap().user_id))
         .await
         .map_err(|e| {
-            tracing::error!("Error: {}", e);
+            tracing::error!("purchase_file_query Error: {}", e);
             Error::new(ErrorKind::Other, "DB Query failed")
         })?;
 
-    // let response: Option<_> = purchase_file_query.take(0).map_err(|e| {
-    //     tracing::error!("Error: {}", e);
-    //     Error::new(ErrorKind::Other, "UploadedFile deserialization failed")
-    // })?;
+    let _response: Option<String> = purchase_file_query.take(0).map_err(|e| {
+        tracing::error!("purchase_file_query Deserialization Error: {}", e);
+        Error::new(ErrorKind::Other, "UploadedFile deserialization failed")
+    })?;
 
     Ok(true)
 }
