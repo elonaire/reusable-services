5c1daa969e41297b6faea3ca1faf5d1822e00292
Elon Aseneka <elonsantos63@gmail.com>
2025-05-07 17:41:30 +0300

    rectify longstanding gRPC OUT_DIR issue

--- lib/build.rs
+++ lib/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = "src/integration/grpc/out";
-    std::env::set_var("PROTOBUFS", out_dir);
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
